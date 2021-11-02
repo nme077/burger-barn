b4c6dd36d4fe607d0bccd2b881fd867bcd242c06
Nicholas Eveland <nmemusic77@gmail.com>
2021-11-01 22:22:39 -0600

    Troubleshooting Heroku

--- app.js
+++ app.js
@@ -30,9 +30,11 @@ app.get('/api/getMenu', (req, res) => {
 
 //production mode
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static('/app/client/build'));
+    //app.use(express.static(path.join(__dirname, '/client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));  
+        res.sendFile('/app/client/build/index.html');
+        //res.sendFile(path.join(__dirname, '/client/build', 'index.html'));  
     })
 }
 
