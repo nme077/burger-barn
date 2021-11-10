2ed210914ea091a003ce94b173e336246cc84f34
Nicholas Eveland <nmemusic77@gmail.com>
2021-11-09 22:05:47 -0700

    Add fix for react-router/Heroku

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
+app.get("/*", (req, res) => {
+    let url = path.join(__dirname, './client/build', 'index.html');
     if (!url.startsWith('/app/')) // since we're on local windows
       url = url.substring(1);
     res.sendFile(url);
