a3118b4659c1700b02945f95ed99a95991deda05
Nicholas Eveland <nmemusic77@gmail.com>
2021-11-18 20:48:36 -0700

    troubleshooting heroku deploy

--- app.js
+++ app.js
@@ -30,11 +30,9 @@ app.get('/api/getMenu', (req, res) => {
 
 app.get('/*', function (req, res) {
     //production mode
-if(process.env.NODE_ENV === 'production') {  
-    //  app.use(express.static('/app/client/build'));
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build'))); 
-    app.get('*', (req, res) => {   
-        //  res.sendFile('/app/client/build/index.html');
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/client/build', 'index.html'));  
     })
 }
